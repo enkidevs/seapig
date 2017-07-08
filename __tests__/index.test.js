ce7b927d5a4b220ba23d461deb09d073aa3a8e7f
nem035 <n.stojanovic035@gmail.com>
2017-07-08 19:13:52 -0400

    Convert to schema api

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,142 +1,84 @@
 /* globals describe test expect */
-import React from 'react';
-import seapig, { DEFAULT_CHILD } from '../src/index';
+import React from 'react'
+import seapig, { OPTIONAL, REQUIRED } from '../src/index'
 
 // test data
-const PROP = 'whatever';
-const child = <div whatever />;
-const defaultChild = <div />;
-const children = [child, defaultChild];
-const childrenWithSameProps = [child, child];
-const multipleDefaultChildren = [defaultChild, defaultChild];
-const defaultChildArray = [defaultChild];
+const PROP = 'whatever'
+const a = <div whatever />
+const b = <div whatever />
+const c = <div />
+const d = <div />
+const children = [a, b, c, d]
+const childrenWithProp = [a, b]
+const childrenWithoutProp = [c, d]
 
 // error messages
-const ERROR_MSG_MISSING_CHILD = `Missing required prop \`${PROP}\``;
-const ERROR_MSG_MISSING_DEFAULT_CHILD = 'Missing a prop-less child';
-const ERROR_MSG_REPEATING_CHILD = `There can only be 1 child with \`${PROP}\` prop.`;
-const ERROR_MSG_REPEATING_DEFAULT_CHILD =
-  'There can only be 1 child without any of the specified props';
-const ERROR_MSG_INVALID_OPTIONS =
-  '`optional` and `required` props must be an array';
-
-/*
- Note: must use `React.Children.toArray` when comparing instances.
- The reason is that `seapig` uses `React.Children.toArray` under the
- hood which adds a key to each element in the array it returns.
- This means must do the same to make sure keys are equal when comparing.
-*/
-const childrenReact = React.Children.toArray(children);
-const defaultChildArrayReact = React.Children.toArray(defaultChildArray);
+const ERROR_MSG_INVARIANT_MIN = new RegExp(
+  `Must have at least \\d \`${PROP}\` elements`
+)
+const ERROR_MSG_INVARIANT_MAX = new RegExp(
+  `Cannot have more than \\d \`${PROP}\` elements`
+)
 
 describe('seapig', () => {
-  // optional child
-  test('should return optional child that exists', () => {
+  test(`should return array of children matching \`${PROP}\` and an array of the remaining children under \`rest\``, () => {
+    // OPTIONAL
     expect(
       seapig(children, {
-        optional: [PROP],
+        [PROP]: OPTIONAL
       })
-    ).toEqual({ [PROP]: childrenReact[0] });
-  });
-  test('should not return optional child that does not exists', () => {
-    expect(
-      seapig([], {
-        optional: [PROP],
-      })
-    ).toEqual({});
-  });
-  test('should throw for multiple optional children with same prop', () => {
-    expect(() => {
-      seapig(childrenWithSameProps, {
-        optional: [PROP],
-      });
-    }).toThrow(ERROR_MSG_REPEATING_CHILD);
-  });
-
-  // optional default child (without an existing prop)
-  test('should return optional default child that exists', () => {
-    expect(
-      seapig(defaultChildArray, {
-        optional: [DEFAULT_CHILD],
-      })
-    ).toEqual({ [DEFAULT_CHILD]: defaultChildArrayReact[0] });
-  });
-  test('should not return optional default child that does not exists', () => {
-    expect(
-      seapig([], {
-        optional: [DEFAULT_CHILD],
-      })
-    ).toEqual({});
-  });
-  test('should throw for multiple default children', () => {
-    expect(() => {
-      seapig(multipleDefaultChildren, {
-        optional: [DEFAULT_CHILD],
-      });
-    }).toThrow(ERROR_MSG_REPEATING_DEFAULT_CHILD);
-  });
-
-  // required child
-  test('should return required child that exists', () => {
+    ).toEqual({
+      [PROP]: childrenWithProp,
+      rest: childrenWithoutProp
+    })
+    // REQUIRED
     expect(
       seapig(children, {
-        required: [PROP],
+        [PROP]: REQUIRED
       })
-    ).toEqual({ [PROP]: childrenReact[0] });
-  });
-  test('should throw for required child that does not exists', () => {
-    expect(() => {
-      seapig([], {
-        required: [PROP],
-      });
-    }).toThrow(ERROR_MSG_MISSING_CHILD);
-  });
-  test('should throw for multiple required children with same prop', () => {
-    expect(() => {
-      seapig(childrenWithSameProps, {
-        required: [PROP],
-      });
-    }).toThrow(ERROR_MSG_REPEATING_CHILD);
-  });
+    ).toEqual({
+      [PROP]: childrenWithProp,
+      rest: childrenWithoutProp
+    })
+  })
 
-  // required default child (without existing prop)
-  test('should return required default child that exists', () => {
-    expect(
-      seapig(defaultChildArray, {
-        required: [DEFAULT_CHILD],
+  // min and max invariants
+  test(`should throw for violated min invariant`, () => {
+    expect(() => {
+      seapig(childrenWithoutProp, {
+        [PROP]: REQUIRED
       })
-    ).toEqual({ [DEFAULT_CHILD]: defaultChildArrayReact[0] });
-  });
-  test('should throw for required default child that does not exists', () => {
+    }).toThrow(ERROR_MSG_INVARIANT_MIN)
+  })
+  test(`should throw for violated max invariant`, () => {
     expect(() => {
-      seapig([], {
-        required: [DEFAULT_CHILD],
-      });
-    }).toThrow(ERROR_MSG_MISSING_DEFAULT_CHILD);
-  });
-  test('should throw for multiple require default children', () => {
-    expect(() => {
-      seapig(multipleDefaultChildren, {
-        optional: [DEFAULT_CHILD],
-      });
-    }).toThrow(ERROR_MSG_REPEATING_DEFAULT_CHILD);
-  });
+      seapig(childrenWithProp, {
+        [PROP]: {
+          max: 1
+        }
+      })
+    }).toThrow(ERROR_MSG_INVARIANT_MAX)
+  })
 
   // invalid inputs
   test('should return empty object for no inputs', () => {
-    expect(seapig()).toEqual({});
-    expect(seapig(children)).toEqual({});
-  });
-  test('should throw for non-array options props', () => {
+    expect(seapig()).toEqual({
+      rest: []
+    })
+  })
+  test('should return all children as `rest` object for no schema', () => {
+    expect(seapig(children)).toEqual({
+      rest: children
+    })
+  })
+  test('should return empty array under `rest` for invalid children', () => {
+    expect(seapig(null)).toEqual({
+      rest: []
+    })
+  })
+  test('should throw for invalid schema', () => {
     expect(() => {
-      seapig(children, {
-        optional: null,
-      });
-    }).toThrow(ERROR_MSG_INVALID_OPTIONS);
-    expect(() => {
-      seapig(children, {
-        required: null,
-      });
-    }).toThrow(ERROR_MSG_INVALID_OPTIONS);
-  });
-});
+      seapig(children, null)
+    }).toThrow('schema must be an object')
+  })
+})
