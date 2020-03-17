1856b1e70fae9c4f627f7f7c66bb247fb74edfab
Stefano Bourscheid <stefano.bourscheid@gmail.com>
2020-03-17 15:00:12 +0100

    🖋  Add typescript type declarations

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+declare module 'seapig' {
+  import { ReactElement } from 'react';
+
+  interface ValidationConfiguration {
+    min?: number;
+    max?: number;
+  }
+
+  interface Schema {
+    [key: string]: ValidationConfiguration;
+  }
+
+  interface Result {
+    [key: string]: ReactElement[];
+    rest: ReactElement[];
+  }
+
+  export default function seapig(
+    children: ReactElement,
+    schema: Schema,
+  ): Result;
+
+  export const OPTIONAL: ValidationConfiguration;
+
+  export const OPTIONALS: ValidationConfiguration;
+
+  export const REQUIRED: ValidationConfiguration;
+
+  export const REQUIREDS: ValidationConfiguration;
+}
